fcc4172a8b5a7aacb53eec0a96a950bc5e248586
klapka.ota@gmail.com <klapka.ota@gmail.com>
2015-11-26 17:40:28 +0100

    just created

---
+++ src/core/Controller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Otas
+ * Date: 26. 11. 2015
+ * Time: 17:34
+ */
+
+namespace core;
+
+
+class Controller
+{
+    public function __construct()
+    {
+
+    }
+}
